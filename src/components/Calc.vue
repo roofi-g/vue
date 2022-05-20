0d14e84239b5cfa67f8cfaa2240ec1ee9ca60c52
Руфина <roofi.g@yandex.ru>
2022-05-20 18:09:51 +0500

    homework 1

---
+++ src/components/Calc.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="display">
+      <input v-model.number="operand1" type="number">
+      <input v-model.number="operand2" type="number">
+      = {{ result }}
+    </div>
+    <div class="keyboard">
+      <button @click="sum">+</button>
+      <button @click="sub">-</button>
+      <button @click="mult">*</button>
+      <button @click="div">/</button>
+      <button @click="expo">**</button>
+      <button @click="remainder">%</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalcComponent',
+  data () {
+    return {
+      operand1: 0,
+      operand2: 0,
+      result: 0
+    }
+  },
+  methods: {
+    sum () {
+      this.result = this.operand1 + this.operand2
+    },
+    sub () {
+      this.result = this.operand1 - this.operand2
+    },
+    mult () {
+      this.result = this.operand1 * this.operand2
+    },
+    div () {
+      this.result = this.operand1 / this.operand2
+    },
+    expo () {
+      this.result = this.operand1 ** this.operand2
+    },
+    remainder () {
+      this.result = this.operand1 % this.operand2
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CalcComponent/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import CalcComponent from '@/components/Calc.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    CalcComponent
   }
 }
 </script>
